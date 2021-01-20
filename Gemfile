2b97ba70e3d19fad5558a03a1ea54d35c532b1d5
Daniel Dvorkin <dvorkin212@gmail.com>
2021-01-20 18:49:44 -0500

    fixed up the login and signup screens

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'devise'
 gem "haml-rails"
+gem "administrate"
+
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'visitor#index'
